f11af34d6a531ae7504e9f8a04867f34b24cf955
eyan422 <eyan422eyan422@gmail.com>
2013-03-01 23:20:03 +0800

    Get & put

--- Chapter1/Triplet.c
+++ Chapter1/Triplet.c
@@ -9,14 +9,36 @@ typedef ElemType *Triplet;
 
 Status InitTriplet(Triplet *T,ElemType v1,ElemType v2,ElemType v3);
 Status DestroyTriplet(Triplet *T);
+Status Get(Triplet T,int i,ElemType *e);
+Status Put(Triplet *T,int i,ElemType e);
 
 int main()
 {
 	Triplet Pointer = NULL;
+	ElemType e = 0;
 	InitTriplet(&Pointer,1,2,3);
-	printf("%d\n",*Pointer);
+	
+	Get(Pointer,1,&e);
+	printf("e1:%d\n",e);
+	
+	Put(&Pointer,1,5);
+	Get(Pointer,1,&e);
+	
+	printf("e1:%d\n",e);
+	
 	DestroyTriplet(&Pointer);
 	return 0;
+}
+
+Status Put(Triplet *T,int i,ElemType e)
+{
+	if( i < 0 || i >3 )
+	{
+		printf("i is out of range[0-2]");
+		exit(ERROR);
+	}
+
+	(*T)[i] = e;
 }
 
 Status InitTriplet(Triplet *T,ElemType v1,ElemType v2,ElemType v3)
@@ -30,10 +52,10 @@ Status InitTriplet(Triplet *T,ElemType v1,ElemType v2,ElemType v3)
 	}
 	
 	(*T)[0] = v1;
-	//T[1] = v2;
-	//T[2] = v3;
+	(*T)[1] = v2;
+	(*T)[2] = v3;
 
-	printf("Alllcation is successful\n");
+	printf("Allocation is successful\n");
 	
 	return OK;
 }
@@ -42,6 +64,21 @@ Status DestroyTriplet(Triplet *T)
 {
 	free(*T);
 	*T = NULL;
+	
+	printf("Dellocation is successful\n");
 
+	return OK;
+}
+
+Status Get(Triplet T,int i,ElemType *e)
+{
+	if( i < 0 || i > 3)
+	{
+		printf("i is out of range[0-2]");
+		exit(ERROR);
+	}
+
+	*e = T[i];
+	
 	return OK;
 }
