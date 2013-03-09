58eda283d4e3ef1b9a7ce2c17fff822447aa0f71
eyan422 <eyan422eyan422@gmail.com>
2013-03-09 16:50:34 +0800

    half way

--- Chapter2/polynomial.c
+++ Chapter2/polynomial.c
@@ -1,7 +1,74 @@
 #include <Common.h>
 
+typedef struct term
+{
+	float ceof;
+	int expn;
+}term,Elem;
+
+typedef struct pnode
+{
+	Elem data;
+	struct pnode *next;
+}pnode,*linklist;
+
+typedef linklist polynomail;
+
+Status CreatePolyn(polynomail *p,int ceof,int expn);
+Status DestroyPolyn(polynomail *head)
+void traverse(polynomail head,void (*func)());
+void print(polynomail head);
+
 int main()
 {
+	polynomail head;
+	CreatePolyn(&head,0,0);
+	DestroyPolyn(&head);	
+	return OK;
+}
+
+void print(polynomail head)
+{
+
+}
+
+void traverse(polynomail head,void (*func)())
+{
+	polynomail index = head;
 	
-	return ok;
+	while(index != NULL)
+	{
+		func(index);
+		index = index->next;
+	}
+}
+
+Status CreatePolyn(polynomail *p,int ceof,int expn)
+{
+	polynomail index = *p;
+	
+	index = (polynomail)malloc(sizeof(pnode));
+	if(index != NULL)
+	{
+		index->next = NULL;
+		(index->data).ceof = ceof;
+		(index->data).expn = expn;
+	
+		return OK;
+	}
+	return ERROR;
+}
+
+
+Status DestroyPolyn(polynomail *head)
+{
+	polynmail index = head;
+	polynmail tmp = head;
+
+	while(index != NULL)
+	{
+		tmp = index->next;
+		free(index);
+		index = tmp;
+	}
 }
