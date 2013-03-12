16e243738e51e86a749f105d71329d49d12ea626
eyan422 <eyan422eyan422@gmail.com>
2013-03-12 23:08:43 +0800

    struct pointer

--- Chapter2/polynomial.c
+++ Chapter2/polynomial.c
@@ -1,21 +1,107 @@
 #include <Common.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct dat
+typedef struct
 {
-	int e;
-	int b;
+	float 	coef;
+	int 	expn;
 }ElemType;
 
-typedef struct node
+typedef struct
 {
 	ElemType data;
 	struct node *next;
-}node,*ListPointer;
+}node,*LinkList;
 
-Status create();
+typedef LinkList polynomial;
+
+void CreatePolyn(polynomial *pointer, int m);
+void DestroyPolyn(polynomial *pointer);
+void Traverse(polynomial pointer, void (*func)());
+void printPoly(polynomial pointer);
 
 int main()
-{
+{	
+	polynomial p;
+	(p->data).coef = 2.0;
+	(p->data).expn = 3;
 	
+	printf("%fX^%d ",(p->data).coef,(p->data).expn);
+	
+	//CreatePolyn(&p,2);
+	//Traverse(p,printPoly);
+	//DestroyPolyn(&p);
 	return OK;
+}
+
+void printPoly(polynomial pointer)
+{
+	printf("%fX^%d ",(pointer->data).coef,(pointer->data).expn);
+}
+
+void Traverse(polynomial pointer, void (*func)())
+{
+	polynomial index = pointer->next;
+	
+	while(index != NULL)
+	{
+		//func(pointer);
+		//index = index->next;
+	}
+}
+
+void CreatePolyn(polynomial *pointer, int m)
+{
+	int count = m;
+	float coef;
+	int expn;
+	polynomial index = *pointer;
+	polynomial tmp = NULL;
+	
+		
+	index = (polynomial)malloc(sizeof(node));
+	if(index != NULL)
+	{
+		index->next = NULL;
+		(index->data).coef = 0;
+		(index->data).expn = 0;
+	}
+
+	for(count = 0; count < m; count++)
+	{
+		tmp = (polynomial)malloc(sizeof(node));
+		if(tmp != NULL)
+		{
+			printf("please input the coef:\n");
+			scanf("%f",&coef);
+			printf("please input the expn:\n");
+			scanf("%d",&expn);
+
+			tmp->next = index->next;
+			index->next = tmp;
+			//index = tmp;
+		
+			(tmp->data).coef = coef;
+			(tmp->data).expn = expn;
+		}
+	}
+	printf("Create succeeds\n");
+}
+
+
+void DestroyPolyn(polynomial *pointer)
+{
+	polynomial tmp = NULL;
+	polynomial index = *pointer;
+
+	while(index != NULL)
+	{
+		tmp = index->next;
+		free(index);
+		index = tmp;
+	}
+	
+	printf("free succeeds\n");
 }
