22f789980f3c2518386887c9aa4239d2a7b92be8
eyan422 <eyan422eyan422@gmail.com>
2013-03-12 10:50:29 +0800

    poly

--- Chapter2/polynomial.c
+++ Chapter2/polynomial.c
@@ -1,7 +1,21 @@
 #include <Common.h>
 
+typedef struct dat
+{
+	int e;
+	int b;
+}ElemType;
+
+typedef struct node
+{
+	ElemType data;
+	struct node *next;
+}node,*ListPointer;
+
+Status create();
+
 int main()
 {
 	
-	return ok;
+	return OK;
 }
